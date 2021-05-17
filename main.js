bc8f41ac36d8727958b1b5deee7ab56b0603730b
Amy Gori <amydgori@gmail.com>
2021-05-17 15:51:15 -0400

    Add examples from class

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log('hello world!')
+// let score = 0
+// const player = 'Amy'
+// console.log('On line 4, score is ', score)
+// score += 10
+// console.log('On line 6, score is ', score)
+
+// const b = 5
+// let a = b * 2
+// console.log('I got to line 9')
+// console.log(a)
+
+function hello(input) {
+  return 'Hi, ' + input + '!'
+}
+
+let username = 'Ellie'
+let greeting = hello(username)
+
+console.log(greeting)
